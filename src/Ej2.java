8900e86746c84dc72d59126b7345a01c5292b29f
CrazyNil <stevemolineroquintana@gmail.com>
2024-09-26 16:53:53 +0200

    Avances ej2

--- src/Ej2.java
+++ src/Ej2.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Ej2 {
+    public static <E> void modifyPrefix(LinkedList<E> src, LinkedList<E> trg)
+    {
+        for (int i = 0; i < trg.size(); i++)
+        {
+            if (i < src.size())
+            {
+                trg.set(i,src.get(i));
+            }
+            else
+            {
+                break;
+            }
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Character> src = new LinkedList<>(Arrays.asList('a', 'b', 'c', 'd'));
+        LinkedList<Character> trg = new LinkedList<>(Arrays.asList('x', 'y', 'z','0','1'));
+        modifyPrefix(src,trg);
+    }
 }
