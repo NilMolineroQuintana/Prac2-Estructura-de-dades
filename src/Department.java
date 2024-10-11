1bcb79ad25b7b3e70f6725216ed68813d71b57b3
GuillemAlcoverro <gal7@alumnes.udl.cat>
2024-10-11 21:27:34 +0200

    Department.java Commented

--- src/Department.java
+++ src/Department.java
@@ -1,11 +1,11 @@
 public class Department {
-    private Engineer head;
+    private Engineer head; // Guardem el jefe del departament
 
     public Department(Engineer head) {
-        this.head = head;
+        this.head = head; // Asignar el enginyer introduït com a jefe del departament
     }
 
-    public Engineer getHead() {
+    public Engineer getHead() { // Retorna el jefe del departament
         return head;
     }
 }
